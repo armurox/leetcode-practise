52b5fa7a85a098027587c033b694ae1558c36690
Arman Jasuja <114303348+armurox@users.noreply.github.com>
2026-06-25 00:16:58 +0400

    Add: trapping-rain-water - submission-2

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-2.py
@@ -0,0 +1,30 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        """
+        Time complexity of this solution is O(n) and space complexity is O(n)
+        The important realization here is that the max trappable area by any endpoint
+        is the min(max_left_of_position, min_right_of_position) - height_of_position
+        (at a minimum of 0).
+        """
+        area = 0
+        # construct max_left array
+        max_left = [0] * len(height)
+        max_val = 0
+        height_size = len(height)
+        for i in range(height_size):
+            max_left[i] = max_val
+            if height[i] > max_val:
+                max_val = height[i]
+        # construct max_right array
+        max_right = [0] * height_size
+        max_val = 0
+        for i in reversed(range(height_size)):
+            max_right[i] = max_val
+            if height[i] > max_val:
+                max_val = height[i]
+        # find the total max area trappable
+        for i in range(height_size):
+            # add on the max area trappable by the current position
+            curr_position_area = min(max_left[i], max_right[i]) - height[i]
+            area += curr_position_area if curr_position_area >= 0 else 0
+        return area
